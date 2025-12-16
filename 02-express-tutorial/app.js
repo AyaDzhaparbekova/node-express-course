253446a90fe44d4514f79a85849982968b718703
Aya D <dzhaicholpon@gmail.com>
2025-12-16 11:11:58 -0600

    middleware postman

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,46 @@
-const { products } = require('./data');
-
 const express = require('express');
 const app = express();
 
-app.use(express.static('./public'));
+const { products, people } = require('./data');
+
+//middleware
+const logger = (req, res, next) => {
+  console.log(req.method, req.url, new Date().toLocaleTimeString());
+  next();
+};
+
+app.use(logger);
+app.use(express.static('./methods-public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const peopleRouter = require('./routes/people');
+
+
+// app.get('/', (req, res) => res.send('Home'));
+// app.get('/api/v1/people', (req, res) => {
+//   res.json(people);
+// });
+
+// app.post('/api/v1/people', (req, res) => {
+//   if (!req.body.name) {
+//     return res
+//       .status(400)
+//       .json({ success: false, message: 'Please provide a name' });
+//   }
+
+//   people.push({ id: people.length + 1, name: req.body.name });
+
+//   res.status(201).json({ success: true, name: req.body.name });
+// });
+
 
 app.get('/api/v1/test', (req, res) => {
   res.json({ message: 'This is a test API', status: 'success' });
+});
+// home route
+app.get('/', (req, res) => {
+  res.send('Home');
 });
 
 app.get('/api/v1/products', (req, res) => {
@@ -36,6 +70,11 @@ app.get('/api/v1/query', (req, res) => {
     );
   }
 
+  // filter by maxPrice
+  if (maxPrice) {
+    filtered = filtered.filter(p => p.price < Number(maxPrice));
+  }
+
   // limit number of results
   if (limit) {
     filtered = filtered.slice(0, Number(limit));
@@ -44,14 +83,10 @@ app.get('/api/v1/query', (req, res) => {
   res.json(filtered);
 });
 
-if (maxPrice) {
-  filtered = filtered.filter(p => p.price < Number(maxPrice));
-}
-
 app.use((req, res) => {
   res.status(404).send('Page not found');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000...');
+app.listen(3001, () => {
+  console.log('Server is running on port 3001...');
 });
